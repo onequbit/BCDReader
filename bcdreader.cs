b744803176fff37020f5600ab48b4744b2c86a07
Alfred Aquino <alfredaquino@hotmail.com>
2020-06-20 16:33:13 -0400

    trying to clean up repos

--- bcdreader.cs
+++ bcdreader.cs
@@ -11,45 +11,234 @@ using System.Windows.Forms;
 namespace App
 {
 
-    public class Program
+    public enum BcdItemTypeEnum
     {
-        
-        public static void ParseBcdLines(string data)
+        PropertyString,
+        GuidValue,
+        Separator,
+        ObjectTitle,
+        BootLoader,
+        BootManager,
+        ParseError,
+        NotSet
+
+    }
+
+    public class BcdProperty
+    {
+        private string sourceString;
+
+        private Guid guidValue = new Guid("{00000000-0000-0000-0000-000000000000}");
+
+        public BcdProperty(string str)
         {
-            int count = 0;
-            foreach(string line in data.Split('\n'))
+            this.sourceString = str.Trim();
+            this.EnumType = this.GetBcdPropertyType();
+        }
+
+        public string Title;
+
+        public string Key;
+
+        public string Value;
+
+        public bool IsGuid
+        {
+            get
+            {   
+                Guid newGuid;
+                return Guid.TryParse(this.sourceString, out newGuid);                
+            }            
+        }
+
+        public bool IsCollection
+        {
+            get
             {
-                string[] parts = line.Split();
-                if (parts.Length == 1)
-                {
-                    Console.WriteLine($"{count}# *{line}*");
-                }
-                else
-                {
-                    (string key, string[] rest) = parts.Pop();
-                    string value = rest.Join(" ").Trim();
-                    Console.WriteLine($"{count}# '{key}':'{value}'");
-                }
-                count++;
+                return this.EnumType == BcdItemTypeEnum.PropertyString &&
+                        (
+                            this.Key.ToLower().EndsWith("order") ||
+                            this.Key.ToLower().EndsWith("sequence")
+                        );
+                        
             }
         }
+        
+        public BcdItemTypeEnum EnumType = BcdItemTypeEnum.NotSet;
+
+        public bool IsNewObject
+        {
+            get
+            {
+                return (this.EnumType == BcdItemTypeEnum.BootLoader ||
+                        this.EnumType == BcdItemTypeEnum.BootManager);
+            }
+        }
+       
+        public Guid GuidValue
+        {
+            get
+            {
+                return this.guidValue;                
+            }
+        }
+
+        private static string separator(int size)
+        {
+            return new string((char)45, size);
+        }
+
+        private BcdItemTypeEnum GetBcdPropertyType()
+        {
+            string[] parts = this.sourceString.Split(' ');
+            if (parts.Length == 3 && parts[0].Equals("Windows") && parts[1].Equals("Boot"))
+            {
+                this.Title = this.sourceString;
+                if (parts[2].Equals("Loader")) return BcdItemTypeEnum.BootLoader;
+                else if (parts[2].Equals("Manager")) return BcdItemTypeEnum.BootManager;
+                else 
+                return BcdItemTypeEnum.ParseError;
+            }
+            else if (parts.Length == 1)
+            {
+                if (this.IsGuid)
+                {
+                    this.guidValue = Guid.Parse(this.sourceString);
+                    return BcdItemTypeEnum.GuidValue;
+                } 
+                if (this.sourceString.Equals(separator(sourceString.Length)))
+                {                    
+                    return BcdItemTypeEnum.Separator;
+                }
+                else return BcdItemTypeEnum.ParseError;
+            }
+            else
+            {
+                this.Key = parts[0].Trim();
+                this.Value = this.sourceString.Replace(this.Key,"").Trim();
+                return BcdItemTypeEnum.PropertyString;
+            }            
+        }
+
+        public override string ToString()
+        {
+            string name = this.EnumType.Name();
+            if (this.EnumType == BcdItemTypeEnum.BootLoader)
+            {
+                return $"[[{this.Title}]]";
+            }
+            else 
+            if (this.EnumType == BcdItemTypeEnum.PropertyString)
+            {
+                return $"[{this.Key}]: '{this.Value}'";
+            }
+            else             
+            return "";            
+        }
+    }
+
+    public class BcdObject
+    {
+        public string TypeName = "";
+        Dictionary<string,object> Properties = new Dictionary<string, object>();
+
+        public BcdObject()
+        {
+            
+        }
+
+        public static bool IsEmpty(BcdObject obj)
+        {
+            return obj.Properties.Count == 0;
+        }
+
+        public static List<BcdObject> ParseCollection(string data)
+        {            
+            List<BcdObject> collection = new List<BcdObject>();
+            BcdObject obj = new BcdObject();
+            BcdProperty previous = new BcdProperty("---");
+            try
+            {            
+                foreach(string line in data.Split('\n'))
+                {                
+                    if (line.Length > 1)
+                    {                        
+                        BcdProperty bps = new BcdProperty(line);
+                        if (bps.IsNewObject)
+                        {
+                            if (!IsEmpty(obj)) collection.Add(obj);
+                            obj = new BcdObject();
+                            obj.TypeName = bps.Title;
+                        } else
+                        if (bps.EnumType == BcdItemTypeEnum.PropertyString)
+                        {
+                            if (bps.Key.Length > 0 && bps.Value.Length > 0)
+                            {                                
+                                obj.Properties[bps.Key] = bps.Value;
+                                if (bps.IsCollection)
+                                {
+                                    obj.Properties[bps.Key] = new List<Guid>();
+                                    ((List<Guid>)obj.Properties[bps.Key]).Add(new Guid(bps.Value));
+                                    previous = bps;
+                                }                                
+                            }
+                        } else 
+                        if (bps.IsGuid && previous.IsCollection)
+                        {
+                            ((List<Guid>)obj.Properties[previous.Key]).Add(bps.GuidValue);
+                        }
+                    }
+                }
+                collection.Add(obj);                
+            } 
+            catch (Exception ex)
+            {
+                 Console.WriteLine($"{ex.Message}\n{ex.StackTrace}\n{ex.InnerException}");  
+            }
+            return collection;
+        }
+
+        private string GetGuidList(List<Guid> collection)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(var guid in collection)
+            {                
+                sb.Append("{" + $"{guid.ToString()}" + "},");
+            }
+            return sb.ToString().TrimEnd(',');
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(this.TypeName);
+            foreach(string key in this.Properties.Keys)
+            {                
+                string value = this.Properties[key].ToString();
+                if (typeof(List<Guid>) == this.Properties[key].GetType())
+                    value = GetGuidList((List<Guid>)this.Properties[key]);
+                sb.AppendLine($"{key}: '{value}'");
+            }
+            return sb.ToString();
+        }
+    }
+
+    public class BcdReader
+    {            
 
         [STAThread] // required for Windows Forms
         public static void Main(string[] args)
         {            
             try
             {   
-                // this part is finally working...
-                // Lib.CmdWhere(@"Z:\OneDrive\Dev\Pseudo\","pseudo.exe").ToConsole();
-                string path = Lib.SearchEnvPathsFor("Pseudo.exe");
-                Console.WriteLine(path);
-                // string[] pseudoExe = Lib.GetCmdOutput("where /r c: pseudo.exe").Split('\n');
-                // Console.WriteLine($"pseudoExe: {pseudoExe}");
-                // string bcdInfo = Lib.GetPseudoCmdOutput("bcdedit");
-                // ParseBcdLines(bcdInfo);
+                string bcdInfo = Lib.GetPseudoCmdOutput("bcdedit /enum /v");                
+                var objs = BcdObject.ParseCollection(bcdInfo);
                 
-                
-                // Console.WriteLine(bcdInfo);                
+                Console.WriteLine($"{objs.Count} objects parsed");
+                foreach(var obj in objs)
+                {
+                    Console.WriteLine(obj);
+                }
             }
             catch (Exception ex)
             {                
--- define.cmd
+++ define.cmd
@@ -1,11 +1,9 @@
 @echo off
 set outfile=BCDReader.exe
-REM set buildfiles=Win32Dll.cs TempFile.cs AdminProcess.cs ExtensionMethods.cs Notifier.cs ConsoleAttached.cs bcdreader.cs
-set buildfiles=ExtensionMethods.cs Lib.cs bcdreader.cs
-set runelevated=-win32manifest:BCDReader.exe.manifest
+set buildfiles=ExtensionMethods.cs Lib.cs BCDReader.cs
+set runelevated=-win32manifest:%outfile%.manifest
 set strongname=/keyfile:keyfile.snk
 set appicon=-win32icon:onequbit.ico
 set winexeoptions=-target:winexe %appicon% %strongname% %runelevated%
 set consoleoptions=-target:exe %appicon% %strongname%
-REM set consoleoptions=-target:winexe %appicon% %strongname%
 set compileoptions=-out:%outfile% %consoleoptions%
